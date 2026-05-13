3db0e837a5b51c96b581f71b97c1ffbea98cc6e3
rohanre1813 <rohanshetty135781@gmail.com>
2026-05-14 03:02:30 +0530

    Fix sitemap domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://jusbill.online",
+  siteUrl: "https://billing.jusbill.online",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "weekly",
